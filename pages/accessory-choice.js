85fcc5ff31e0bacab70e03c906e020e145741fdd
BTSBRINGMEHERE <mynameisyyb@naver.com>
2022-08-08 17:25:23 +0900

    start / device / accessory
    메인 화면
    장비 선택화면
    액세서리 선택화면
    라디오를 효율적으로 사용하는 법 추가

--- pages/accessory-choice.js
+++ pages/accessory-choice.js
@@ -1,5 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import BottomButton from "../src/components/BottomButton";
+import Radio from "../src/components/Radio";
+import RadioGroup from "../src/components/RadioGroup";
 
 export default function accessoryChoice() {
-  return <div>accessoryChoice</div>;
+	const router = useRouter();
+	// const [selectedAccessory, setSelectedAccessory] = useState("");
+
+	// const onChangeHandler = (e) => {
+	// 	setSelectedAccessory(e.target.value);
+	// };
+
+	return (
+		<div className="container">
+			<div className="wrapper">
+				<h3 className="title">
+					모니터와 키보드 중 원하는 악세서리를 선택하세요.
+				</h3>
+
+				{/* <label>
+					<input
+						type="radio"
+						name="accessory"
+						id="accessory"
+						value="moniter"
+						onChange={onChangeHandler}
+					/>
+					moniter
+				</label>
+				<label>
+					<input
+						type="radio"
+						name="accessory"
+						id="accessory"
+						value="keyboard"
+						onChange={onChangeHandler}
+					/>
+					keyboard
+				</label> */}
+
+				<RadioGroup name="accessory" onChange={(state) => console.log(state)}>
+					<Radio
+						id="moniter"
+						name="accessory"
+						value="moniter"
+						label="moniter"
+					/>
+					<Radio
+						id="keyboard"
+						name="accessory"
+						value="keyboard"
+						label="keyboard"
+					/>
+				</RadioGroup>
+				<BottomButton onClick={() => router.push("/complete")}>
+					악세서리 선택 화면으로 넘어가기
+				</BottomButton>
+			</div>
+		</div>
+	);
 }
--- pages/device-choice.js
+++ pages/device-choice.js
@@ -1,5 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import BottomButton from "../src/components/BottomButton";
+import Radio from "../src/components/Radio";
+import RadioGroup from "../src/components/RadioGroup";
 
 export default function deviceChoice() {
-  return <div>deviceChoice</div>;
+	const router = useRouter();
+	// const [selectedDevice, setSelectedDevice] = useState("");
+
+	// const onChangeHandler = (e) => {
+	// 	setSelectedDevice(e.target.value);
+	// };
+
+	return (
+		<div className="container">
+			<div className="wrapper">
+				<h3 className="title">아이맥과 맥북 중 원하는 장비를 선택하세요.</h3>
+
+				{/* <label>
+					<input
+						type="radio"
+						name="device"
+						id="device"
+						value="iMac"
+						onChange={onChangeHandler}
+					/>
+					iMac
+				</label>
+				<label>
+					<input
+						type="radio"
+						name="device"
+						id="device"
+						value="Macbook"
+						onChange={onChangeHandler}
+					/>
+					Macbook
+				</label> */}
+
+				<RadioGroup name="device" onChange={(state) => console.log(state)}>
+					<Radio id="iMac" name="device" value="iMac" label="iMac" />
+					<Radio id="Macbook" name="device" value="Macbook" label="Macbook" />
+				</RadioGroup>
+
+				<BottomButton onClick={() => router.push("/accessory-choice")}>
+					악세서리 선택 화면으로 넘어가기
+				</BottomButton>
+			</div>
+		</div>
+	);
 }
